0dec1d7911070bd1da811b0a317a2f5744409710
Mujahid <mujahidramdhani@gmail.com>
2025-03-08 14:34:24 +0700

    change message

--- src/app/validators/auth.ts
+++ src/app/validators/auth.ts
@@ -1,33 +1,34 @@
 import Joi from "joi";
 export const registerSchema = Joi.object({
-  fullName: Joi.string().min(3).required().messages({
-    "string.min": "Nama lengkap minimal 3 karakter",
-    "string.empty": "Nama lengkap harus diisi",
-  }),
-  email: Joi.string()
-    .email({ tlds: { allow: false } })
-    .required()
-    .messages({
-      "string.email": "Email tidak valid",
-      "string.empty": "Email harus diisi",
-    }),
-  password: Joi.string().min(6).required().messages({
-    "string.min": "Password minimal 6 karakter",
-    "string.empty": "Password harus diisi",
-  }),
-  confirmPassword: Joi.string().valid(Joi.ref("password")).required().messages({
-    "any.only": "Konfirmasi password tidak cocok",
-    "string.empty": "Konfirmasi password harus diisi",
-  }),
+fullName: Joi.string().min(3).required().messages({
+"string.min": "Full name must be at least 3 characters long",
+"string.empty": "Full name is required",
+}),
+email: Joi.string()
+.email({ tlds: { allow: false } })
+.required()
+.messages({
+"string.email": "Invalid email format",
+"string.empty": "Email is required",
+}),
+password: Joi.string().min(6).required().messages({
+"string.min": "Password must be at least 6 characters long",
+"string.empty": "Password is required",
+}),
+confirmPassword: Joi.string().valid(Joi.ref("password")).required().messages({
+"any.only": "Password confirmation does not match",
+"string.empty": "Password confirmation is required",
+}),
 });
 
 export const LoginSchema = Joi.object({
-  email: Joi.string().email({ tlds: { allow: false } }).required().messages({
-    "string.email": "Email tidak valid",
-    "string.empty": "Email harus diisi",
-  }),
-  password: Joi.string().min(6).required().messages({
-    "string.min": "Password minimal 6 karakter",
-    "string.empty": "Password harus diisi",
-  }),
+email: Joi.string().email({ tlds: { allow: false } }).required().messages({
+"string.email": "Invalid email format",
+"string.empty": "Email is required",
+}),
+password: Joi.string().min(6).required().messages({
+"string.min": "Password must be at least 6 characters long",
+"string.empty": "Password is required",
+}),
 });
+
